1cf4df6dd4f252a0deddf7aad48ab1d204920c76
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-10-17 03:02:47 +0900

    안토니오 당신을 저주해

--- jira-clone/src/features/auth/components/sign-in-card.tsx
+++ jira-clone/src/features/auth/components/sign-in-card.tsx
@@ -1,5 +1,7 @@
 import { z } from "zod";
 
+import Link from "next/link";
+
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { useForm } from "react-hook-form";
@@ -106,6 +108,17 @@ export const SignInCard = () => {
           <FaGithub className="mr-2 size-5" />
           Login with Github
         </Button>
+      </CardContent>
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7 flex items-center justify-center">
+        <p>
+          Don&apos;t have an account?
+          <Link href="/sign-up">
+            <span className="text-blue-700"> Sign Up</span>
+          </Link>
+        </p>
       </CardContent>
     </Card>
   );
