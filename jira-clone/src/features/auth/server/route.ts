06b08fad6b10a5e63cf1d8af0359269a6ccc2724
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-10-21 21:35:44 +0900

    커밋

--- jira-clone/src/features/auth/server/route.ts
+++ jira-clone/src/features/auth/server/route.ts
@@ -1,19 +1,22 @@
-import { z } from "zod";
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 
-const app = new Hono().post(
-  "/login",
-  zValidator(
-    "json",
-    z.object({
-      email: z.string().email(),
-      password: z.string(),
-    })
-  ),
-  (c) => {
-    return c.json({ succes: "ok" });
-  }
-);
+import { loginSchema, registerSchema } from "../schemas";
+
+const app = new Hono()
+  .post("/login", zValidator("json", loginSchema), async (c) => {
+    const { email, password } = c.req.valid("json");
+
+    console.log({ email, password });
+
+    return c.json({ email, password });
+  })
+  .post("/register", zValidator("json", registerSchema), async (c) => {
+    const { name, email, password } = c.req.valid("json");
+
+    console.log({ name, email, password });
+
+    return c.json({ name, email, password });
+  });
 
 export default app;
