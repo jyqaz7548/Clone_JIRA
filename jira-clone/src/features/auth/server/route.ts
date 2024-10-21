7de10e1dcf71cbc27577476e4234be5568c9a3ed
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-10-21 20:12:29 +0900

    혼틈 커밋

--- jira-clone/src/features/auth/server/route.ts
+++ jira-clone/src/features/auth/server/route.ts
@@ -1,7 +1,19 @@
+import { z } from "zod";
 import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
 
-const app = new Hono().post("/login", (c) => {
-  return c.json({ succes: "ok" });
-});
+const app = new Hono().post(
+  "/login",
+  zValidator(
+    "json",
+    z.object({
+      email: z.string().email(),
+      password: z.string(),
+    })
+  ),
+  (c) => {
+    return c.json({ succes: "ok" });
+  }
+);
 
 export default app;
