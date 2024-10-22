49809a6ad42fbe12022e0d97c5022afcb4485f1c
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-10-22 19:23:19 +0900

    commit

---
+++ jira-clone/src/features/auth/constants.ts
@@ -0,0 +1,1 @@
+export const AUTH_COOKIE = "jyqaz-jira-clone-session";
--- jira-clone/src/features/auth/server/route.ts
+++ jira-clone/src/features/auth/server/route.ts
@@ -7,6 +7,10 @@ import { createAdminClient } from "@/lib/appwrite";
 
 import { ID } from "node-appwrite";
 
+import { deleteCookie, setCookie } from "hono/cookie";
+
+import { AUTH_COOKIE } from "../constants";
+
 const app = new Hono()
   .post("/login", zValidator("json", loginSchema), async (c) => {
     const { email, password } = c.req.valid("json");
@@ -21,6 +25,16 @@ const app = new Hono()
     const { account } = await createAdminClient();
 
     await account.create(ID.unique(), email, password, name);
+
+    const session = await account.createEmailPasswordSession(email, password);
+
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30,
+    });
 
     return c.json({ name, email, password });
   });
