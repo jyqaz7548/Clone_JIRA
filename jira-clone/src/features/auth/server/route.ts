cfee7a097d6086fe5f06801f639469620bf2ad9e
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-10-22 19:33:37 +0900

    commit

--- jira-clone/src/features/auth/server/route.ts
+++ jira-clone/src/features/auth/server/route.ts
@@ -15,7 +15,17 @@ const app = new Hono()
   .post("/login", zValidator("json", loginSchema), async (c) => {
     const { email, password } = c.req.valid("json");
 
-    console.log({ email, password });
+    const { account } = await createAdminClient();
+
+    const session = await account.createEmailPasswordSession(email, password);
+
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30,
+    });
 
     return c.json({ email, password });
   })
@@ -37,6 +47,11 @@ const app = new Hono()
     });
 
     return c.json({ name, email, password });
+  })
+  .post("/logout", (c) => {
+    deleteCookie(c, AUTH_COOKIE);
+
+    return c.json({ success: "oh yeah" });
   });
 
 export default app;
