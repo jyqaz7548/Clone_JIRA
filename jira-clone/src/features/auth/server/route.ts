86c36ac4bf68fd03a89a329a840f96f7f406666b
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-10-28 19:23:01 +0900

    ㅈㄱㄳㄷ

--- jira-clone/src/features/auth/server/route.ts
+++ jira-clone/src/features/auth/server/route.ts
@@ -4,6 +4,7 @@ import { zValidator } from "@hono/zod-validator";
 import { loginSchema, registerSchema } from "../schemas";
 
 import { createAdminClient } from "@/lib/appwrite";
+import { sessionMiddleware } from "@/lib/session-middleware";
 
 import { ID } from "node-appwrite";
 
@@ -12,6 +13,11 @@ import { deleteCookie, setCookie } from "hono/cookie";
 import { AUTH_COOKIE } from "../constants";
 
 const app = new Hono()
+  .get("/current", sessionMiddleware, (c) => {
+    const user = c.get("user");
+
+    return c.json({ data: user });
+  })
   .post("/login", zValidator("json", loginSchema), async (c) => {
     const { email, password } = c.req.valid("json");
 
@@ -48,8 +54,11 @@ const app = new Hono()
 
     return c.json({ name, email, password });
   })
-  .post("/logout", (c) => {
+  .post("/logout", sessionMiddleware, async (c) => {
+    const account = c.get("account");
+
     deleteCookie(c, AUTH_COOKIE);
+    await account.deleteSession("current");
 
     return c.json({ success: "oh yeah" });
   });
--- jira-clone/src/lib/session-middleware.ts
+++ jira-clone/src/lib/session-middleware.ts
@@ -7,9 +7,8 @@ import {
   Models,
   Storage,
   type Account as Accounttype,
-  type Client as Clienttype,
   type Databases as Databasestype,
-  type Models as Modelstype,
+  type Users as Userstype,
   type Storage as Storagetype,
 } from "node-appwrite";
 
@@ -18,21 +17,40 @@ import { createMiddleware } from "hono/factory";
 
 import { AUTH_COOKIE } from "@/features/auth/constants";
 
-export const sessionMiddleware = createMiddleware(async (c, next) => {
-  const client = new Client()
-    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+type AdditionalContext = {
+  Variables: {
+    account: Accounttype;
+    databases: Databasestype;
+    storage: Storagetype;
+    users: Userstype;
+    user: Models.User<Models.Preferences>;
+  };
+};
 
-  const session = getCookie(c, AUTH_COOKIE);
+export const sessionMiddleware = createMiddleware<AdditionalContext>(
+  async (c, next) => {
+    const client = new Client()
+      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
 
-  if (!session) {
-    return c.json({ error: "Unauthorized" }, 401);
+    const session = getCookie(c, AUTH_COOKIE);
+
+    if (!session) {
+      return c.json({ error: "Unauthorized" }, 401);
+    }
+    client.setSession(session);
+
+    const account = new Account(client);
+    const databases = new Databases(client);
+    const storage = new Storage(client);
+
+    const user = await account.get();
+
+    c.set("account", account);
+    c.set("databases", databases);
+    c.set("storage", storage);
+    c.set("user", user);
+
+    await next();
   }
-  client.setSession(session);
-
-  const account = new Account(client);
-  const databases = new Databases(client);
-  const storage = new Storage(client);
-
-  const user = await account.get;
-});
+);
