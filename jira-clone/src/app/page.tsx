2c2dfd321af7c81de891d21e65852d7973782a02
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-10-28 20:10:57 +0900

    외않됨?

--- jira-clone/src/app/page.tsx
+++ jira-clone/src/app/page.tsx
@@ -1,19 +1,25 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Select } from "@/components/ui/select";
+"use client";
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useRouter } from "next/router";
+import { useEffect, useReducer } from "react";
+import { Button } from "react-day-picker";
 
 export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
+
   return (
-    <div className="flex gap-4">
-      <Input />
-      <Select> </Select>
-      <Button size={"xs"}>Primary</Button>
-      <Button variant={"secondary"}>Secondary</Button>
-      <Button variant={"destructive"}>Destructive</Button>
-      <Button variant={"ghost"}>Ghost</Button>
-      <Button variant={"muted"}>Muted</Button>
-      <Button variant={"outline"}>Outline</Button>
-      <Button variant={"teritrary"}>teritary</Button>
+    <div className="">
+      Only visible to authorized users.
+      <Button onClick={() => mutate()}>Logout</Button>
     </div>
   );
 }
