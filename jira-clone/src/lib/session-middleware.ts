133556c1933598cb6f7a31b309c804a80ce537bf
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-10-28 19:00:20 +0900

    으

--- jira-clone/src/lib/session-middleware.ts
+++ jira-clone/src/lib/session-middleware.ts
@@ -17,4 +17,22 @@ import { getCookie } from "hono/cookie";
 import { createMiddleware } from "hono/factory";
 
 import { AUTH_COOKIE } from "@/features/auth/constants";
-import { fromJSON } from "postcss";
+
+export const sessionMiddleware = createMiddleware(async (c, next) => {
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+
+  const session = getCookie(c, AUTH_COOKIE);
+
+  if (!session) {
+    return c.json({ error: "Unauthorized" }, 401);
+  }
+  client.setSession(session);
+
+  const account = new Account(client);
+  const databases = new Databases(client);
+  const storage = new Storage(client);
+
+  const user = await account.get;
+});
