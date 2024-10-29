4601092a3131cc16a253c5bf98debe9dafc29d3e
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-10-29 20:10:37 +0900

    ㅋㅁ

--- jira-clone/src/features/auth/components/user-button.tsx
+++ jira-clone/src/features/auth/components/user-button.tsx
@@ -30,7 +30,7 @@ export const UserButton = () => {
 
   const { name, email } = user;
 
-  const AvatarFallback = name
+  const avatarFallback = name
     ? name.charAt(0).toUpperCase()
     : email.charAt(0).toUpperCase() ?? "U";
 
@@ -50,11 +50,17 @@ export const UserButton = () => {
         sideOffset={10}
       >
         <div className="flex flex-col items-center justify-center gap-2 px-2.5 py-4">
-          <Avatar className="size-10 hover:opacity-75 transition border border-neutral-300">
-            <AvatarFallback className="bg-neutral-200 font-medium text-neutral-500 flex items-center justify-center">
+          <Avatar className="size-[52px] hover:opacity-75 transition border border-neutral-300">
+            <AvatarFallback className="bg-neutral-200 text-xl font-medium text-neutral-500 flex items-center justify-center">
               {avatarFallback}
             </AvatarFallback>
           </Avatar>
+          <div className="flex flex-col items-center justify-center">
+            <p className="text-sm font-medium text-neutral-900">
+              {name || "User"}
+            </p>
+            <p>{email}</p>
+          </div>
         </div>
       </DropdownMenuContent>
     </DropdownMenu>
