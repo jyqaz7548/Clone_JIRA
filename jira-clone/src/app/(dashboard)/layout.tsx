878037f0696a4ad643501a93675c5887aae58b26
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-10-30 20:11:24 +0900

    흑흑

--- jira-clone/src/app/(dashboard)/layout.tsx
+++ jira-clone/src/app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import { Navbar } from "@/components/navbar";
 import { Sidebar } from "@/components/sidebar";
 
 interface DashboardLayoutProps {
@@ -13,7 +14,7 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
         </div>
         <div className="lg:pl-[264px]">
           <div className="mx-auto max-w-screen-2xl h-full">
-            {/* TODO:Navbar*/}
+            <Navbar />
             <main className="h-full py-8 px-6 flex flex-col">{children}</main>
           </div>
         </div>
--- jira-clone/src/components/sidebar.tsx
+++ jira-clone/src/components/sidebar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+
 import { DottedSeparator } from "./dotted-separator";
 import { Navigation } from "./Navigation";
 
