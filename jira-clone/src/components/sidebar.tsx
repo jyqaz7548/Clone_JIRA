0a6500888ef5b261feb8d848e2a35fe2ce5a1fdb
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-10-30 19:26:30 +0900

    아니 로고가 외 안되

---
+++ jira-clone/src/components/sidebar.tsx
@@ -0,0 +1,12 @@
+import { Link } from "lucide-react";
+import Image from "next/image";
+
+export const Sidebar = () => {
+  return (
+    <aside className="h-full bg-neutral-100 p-4 w-full">
+      <Link href="/">
+        <Image src="/logo.svg" alt="logo" width={164} height={48} />
+      </Link>
+    </aside>
+  );
+};
