626e594ab127d4da34cbb9fd2fd6f0c8e6527824
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-10-30 18:06:59 +0900

    컴

--- jira-clone/src/features/auth/components/sign-up-card.tsx
+++ jira-clone/src/features/auth/components/sign-up-card.tsx
@@ -30,7 +30,7 @@ import { registerSchema } from "../schemas";
 import { useRegister } from "../api/use-register";
 
 export const SignUpCard = () => {
-  const { mutate } = useRegister();
+  const { mutate, isPending } = useRegister();
 
   const form = useForm<z.infer<typeof registerSchema>>({
     resolver: zodResolver(registerSchema),
@@ -117,7 +117,7 @@ export const SignUpCard = () => {
                 </FormItem>
               )}
             />
-            <Button disabled={false} size="lg" className="w-full">
+            <Button disabled={isPending} size="lg" className="w-full">
               Register
             </Button>
           </form>
@@ -128,7 +128,7 @@ export const SignUpCard = () => {
       </div>
       <CardContent className=" p-7 flex flex-col gap-y-4">
         <Button
-          disabled={false}
+          disabled={isPending}
           variant="secondary"
           size="lg"
           className="w-full"
@@ -137,7 +137,7 @@ export const SignUpCard = () => {
           Login with Google
         </Button>
         <Button
-          disabled={false}
+          disabled={isPending}
           variant="secondary"
           size="lg"
           className="w-full"
